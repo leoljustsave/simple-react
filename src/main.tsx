ef7a3b3b756267c9a8c9308baaa6415bf4a24c02
luohechen <luoheqp2020@gmail.com>
2021-12-13 20:40:14 +0800

    :wrench:(pkg): remove react-router-config & upgrade react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,15 @@ import { StrictMode } from 'react';
 import { render } from 'react-dom';
 
 import { HashRouter } from 'react-router-dom';
-import { renderRoutes } from 'react-router-config';
-import { routes } from '@/routes';
+import { Routes } from '@/routes';
 
 import '@/asset/scss/reset.scss';
 
 render(
   <StrictMode>
-    <HashRouter>{renderRoutes(routes)}</HashRouter>
+    <HashRouter>
+      <Routes />
+    </HashRouter>
   </StrictMode>,
   document.getElementById('root'),
 );
--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Index } from '@/page/Index';
-
-export const routes = [
-  {
-    path: ['/'],
-    exact: true,
-    component: Index,
-  },
-];
